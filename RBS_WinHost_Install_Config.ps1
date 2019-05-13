cb1e160ead76287af6a0e92becdc3e2a4fd3e4ee
KeiranShelden <keiran.j.shelden@gmail.com>
2019-05-13 23:54:11 +1000

    Update RBS_WinHost_Install_Config.ps1

--- RBS_WinHost_Install_Config.ps1
+++ RBS_WinHost_Install_Config.ps1
@@ -1,20 +1,3 @@
-<#
-This script is to install the Rubrik Backup Service locally and connect it to the Rubrik Cluster for management. 
-
-Example to run:  Set your running directory to where you've copied the .ps1 and environment folder to keep it simple. 
-.\RBS_Install_Config.ps1 -EnvironmentFile .\Environment\Environment.json -OutFile .\RBS.zip 
-
-### If Rubrik module is not installed, it will be installed as part of this script. 
-
-
-(c) Keiran Shelden 2019
-Credit to: 
-Andy Draper for "Volume Filter Driver" Installation API
-vBrownBag API Zero to Hero Series with Chris Wahl and Rebecca Fitzhugh: https://vbrownbag.com/vbrownbag-technology-series/api-zero-to-hero/
-Rubrik Build Community: https://build.rubrik.com/
-
-#>
-
 param(
     [Parameter(Mandatory=$false)]
     [string]$RubrikCluster,
